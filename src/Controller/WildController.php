a8a4aecd99b512af0e511b828e14ea3670eb471e
fouhaha <francois.frk@gmail.com.com>
2020-05-24 12:46:18 +0200

    Quest Symfony04

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -18,4 +18,31 @@ class WildController extends AbstractController
             'website' => 'Bienvenue sur Wild Series',
         ]);
     }
+
+    //@Route("/wild/show/{page}", requirements={"page"="\d+"}, name="wild_show")
+    /**
+     * @Route("/wild/show/{page}",
+     *     requirements={"page"="\d+"},
+     *     defaults={"page"=1},
+     *     name="wild_show"
+     * )
+     */
+    // Même route avec notation différente :
+    // @Route("/wild/show/{page<\d+>?1}", name="wild_show")
+
+
+    // Ici, on a 2 fois la même route avec une écriture différente. Mais le résultat est le même !
+    // @Route("/wild/new", methods={"POST"}, name="wild_new")
+    // Ci-dessus, exemple de redirection via la méthode POST uniquement !
+    // @Route("/wild/{id}", methods={"GET"}, name="wild_show")
+    // Ci-dessus, exemple de redirection via la méthode GET uniquement !
+    // @Route("/wild/{id}", methods={"DELETE"}, name="wild_delete")
+    // Ci-dessus, on limite la méthode à DELETE, pour la suppression d'une série, par ex.
+    // Imposer les méthodes lors de la redirection permet d'éviter la confusion,
+    // lors des redirections sur des mêmes pages !
+    // NB : Il est possible de cumuler les méthodes (par ex : methods={"GET","POST"})
+    public function show(int $page): Response
+    {
+        return $this->render('wild/show.html.twig', ['page' => $page]);
+    }
 }
