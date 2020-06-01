914b2fab498b4d360e9458428b3220426b062fe4
fouhaha <francois.frk@gmail.com.com>
2020-06-01 15:54:01 +0200

    Quest Symfony08

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -3,21 +3,61 @@
 // src/Controller/WildController.php
 namespace App\Controller;
 
+use App\Entity\Program;
+use App\Entity\Category;
+use App\Entity\ProgramRemarque;
+// ATTENTION : La ligne ci-dessus est demandée dans la quête, mais ça doit être une erreur.
+//             En effet, l'Entity ProgramRemarque n'éxistant pas, ce doit être l'entity Program, simplement.
+//             A ce compte-là, la bonne commande "use" est déjà présente ci-dessus !
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\MakerBundle\Str;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
 class WildController extends AbstractController
 {
+//  La methode index() suivante est remplacée par la nouvelle méthode index(), ci-dessous.
+//  ATTENTION : seuls les sommentaires relatifs à la mise en commentaire sont à supprimer si besoin !
+/*
     /**
      * @Route("/wild", name="wild_index")
      */
+/*
     public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+*/
+//  ATTENTION : fin des commentaires relatifs à la mise en commentaire de l'ancienne methode index()
+
+    /**
+     * Show all rows from Program's entity
+     *
+     * @Route("/", name="index")
+     * @return Response A response instance
+     */
+    public function index(): Response
+    {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                "No program found in program's table."
+            );
+        }
+
+        return $this->render(
+            'wild/index.html.twig',
+            ['programs' => $programs]
+        );
     }
 
     //@Route("/wild/show/{page}", requirements={"page"="\d+"}, name="wild_show")
@@ -42,17 +82,96 @@ class WildController extends AbstractController
     // lors des redirections sur des mêmes pages !
     // NB : Il est possible de cumuler les méthodes (par ex : methods={"GET","POST"})
 
-    /**
-     * @Route("/wild/show/{slug}",
-     *     requirements={"slug"="[a-z0-9-]+"},
-     *     defaults={"slug"="aucune sélection"},
-     *     name="wild_show"
-     *  )
-     * @param string $slug
-     * @return Response
-     */
+//  La methode show() suivante est remplacée par la nouvelle méthode show(), ci-dessous.
+//  ATTENTION : seuls les sommentaires relatifs à la mise en commentaire sont à supprimer si besoin !
+    /*
+        /**
+         * @Route("/wild/show/{slug}",
+         *     requirements={"slug"="[a-z0-9-]+"},
+         *     defaults={"slug"="aucune sélection"},
+         *     name="wild_show"
+         *  )
+         * @param string $slug
+         * @return Response
+         */
+/*
     public function show(string $slug): Response
     {
         return $this->render('wild/show.html.twig', ['slug' => ucwords(str_replace("-" , " ", $slug))]);
+    }
+*/
+//  ATTENTION : fin des commentaires relatifs à la mise en commentaire de l'ancienne methode show()
+
+    /**
+     * Getting a program with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     * @Route("/show/{slug<^[a-z0-9-]+$>}", defaults={"slug" = null}, name="show")
+     * @return Response
+     */
+    public function show(?string $slug):Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find a program in program\'s table.');
+        }
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with '.$slug.' title, found in program\'s table.'
+            );
+        }
+
+        return $this->render('wild/show.html.twig', [
+            'program' => $program,
+            'slug'  => $slug,
+        ]);
+    }
+
+    /**
+     * Getting the programs with a formatted categoryName for category
+     *
+     * @param string $categoryName
+     * @Route("/category/{categoryName<^[a-z0-9-]+$>}", defaults={"categoryName" = null}, name="show_category")
+     * @return Response
+     */
+    public function showByCategory(?string $categoryName): Response
+    {
+
+        if (!$categoryName) {
+            throw $this->createNotFoundException(
+                'No program with '.$categoryName.' category, found in program\'s table.'
+            );
+        }
+
+        $categoryName = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($categoryName)), "-")
+        );
+
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => $categoryName]);
+
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(['category' => $category->getId('id')], ['id' => 'desc'], 3, 0);
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No program with '.$categoryName.' category, found in program\'s table.'
+            );
+        }
+
+        return $this->render('wild/category.html.twig', [
+            'programs' => $programs,
+            'categoryName'  => $categoryName,
+        ]);
     }
 }
